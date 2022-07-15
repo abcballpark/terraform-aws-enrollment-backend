0ea758e28e1d28ba219d7bf0a7b4c97330a5258c
Kevin Coffey <kevdougful@gmail.com>
2022-07-15 17:20:48 -0500

    Typo

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "aws_s3_object" "src_zip" {
 
 resource "aws_api_gateway_authorizer" "main" {
   name        = "main"
-  type        = "COGITO_USER_POOLS"
+  type        = "COGNITO_USER_POOLS"
   rest_api_id = aws_api_gateway_rest_api.api.id
   provider_arns = [
     var.user_pool_arn
